b98d8ffda74bddb3404d39b7856ee6f187a53754
Piotr Litwiński <pli@unatrix.com>
2024-11-14 23:20:49 +0100

    getting trades from binance api

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-import * as crypto from "node:crypto";
+import axios from 'axios';
+import crypto from 'crypto';
 
+const API_KEY = "7PyxQOTTkNuSk4hQAROTy7L4QvgNle5ylacptKlpW8u8H3b4YFn7oXxymaZzk32x";
+const API_SECRET = "KCo3vTCsBYjIQbRhXXY7zVYMixY9esWVLpSUHNBYvzr31VfdOoYc9Q6toCFnothR";
+const BASE_URL = "https://testnet.binance.vision/api";
 
-const bytes = crypto.randomBytes(16);
-console.log(bytes);
+const query = `timestamp=${Date.now()}`;
+const signature = crypto.createHmac('sha256', API_SECRET).update(query).digest('hex');
+
+const getTrades = async (symbol) => {
+    try {
+        const trades = await axios.get(`${BASE_URL}/v3/trades`, {
+            headers: {
+                'X-MBX-APIKEY': API_KEY
+            }, params: {
+                symbol,
+                limit: 1
+            }
+        });
+
+        return trades.data;
+    } catch (error) {
+        console.error("Error fetching trades:", error.response ? error.response.data : error.message);
+    }
+
+}
+
+(async () => {
+    const data = await getTrades("BTCUSDT");
+    console.log(data);
+})()
